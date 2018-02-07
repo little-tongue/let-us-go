6cbd6116581ed8604bf95d7446fc57adc39f09e4
biu <hezhou920@gmail.com>
2018-02-07 16:02:38 +0800

    支持手动输入验证码

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const prompt = require('prompt');
 const AipOcrClient = require('baidu-aip-sdk').ocr;
 
 const config = require('./config');
@@ -39,68 +40,106 @@ function isWorth(pet) {
  * @param {string} img - Base64 格式验证码图片
  */
 function recognizeCaptcha(img) {
-  fs.writeFileSync(IMAGE_PATH, img, 'base64');
   return client.accurateBasic(img, { language_type: 'ENG', detect_direction: true });
 }
 
+/**
+ * 获取键盘输入的验证码
+ *
+ * @returns {Promise}
+ */
+function getCaptcha() {
+  return new Promise((resolve, reject) => {
+    prompt.get('captcha', function (err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
 async function letUsGo() {
-  try {
-    // 查询在售的狗
-    let res = await axiosInstance.post(URL_ON_SALE, {
-      'pageNo': 1,
-      'pageSize': 10,
-      'querySortType': 'AMOUNT_ASC',
-      'petIds': [],
-      'lastAmount': null,
-      'lastRareDegree': null,
+  // 查询在售的狗
+  let res = await axiosInstance.post(URL_ON_SALE, {
+    'pageNo': 1,
+    'pageSize': 10,
+    'querySortType': 'AMOUNT_ASC',
+    'petIds': [],
+    'lastAmount': null,
+    'lastRareDegree': null,
+    'requestId': Date.now(),
+    'appId': 1,
+    'tpl': ''
+  });
+  console.log(`第${++count}次查询`);
+
+  const pets = res.data.data.petsOnSale;
+  for (let i = pets.length - 1; i >= 0; i--) {
+    const pet = pets[i];
+    if (!isWorth(pet)) {
+      continue;
+    }
+
+    // 获取验证码
+    res = await axiosInstance.post(URL_CAPTCHA, {
       'requestId': Date.now(),
       'appId': 1,
       'tpl': ''
     });
-    console.log(`第${++count}次查询`);
 
-    const pets = res.data.data.petsOnSale;
-    for (let i = pets.length - 1; i >= 0; i--) {
-      const pet = pets[i];
-      if (!isWorth(pet)) {
-        continue;
-      }
-
-      // 获取验证码
-      res = await axiosInstance.post(URL_CAPTCHA, {
-        'requestId': Date.now(),
-        'appId': 1,
-        'tpl': ''
-      });
-
-      // 识别验证码
-      const captchaDate = res.data.data;
-      const result = await recognizeCaptcha(captchaDate.img);
+    // 识别验证码
+    const captchaData = res.data.data;
+    fs.writeFileSync(IMAGE_PATH, captchaData.img, 'base64');
+    let captcha;
+    if (false) {
+      const result = await recognizeCaptcha(captchaData.img);
       if (result.error_code) {
         console.warn('失败验证码出错', result.error_msg);
         continue;
+      } else {
+        captcha = result.words_result[0].words.replace(/\s/g, '');
+        console.log('成功识别验证码', captcha);
       }
-      const captcha = result.words_result[0].words.replace(/\s/g, '');
-      console.log('成功识别验证码', captcha);
-
-      // 买狗
-      console.log('开始买狗...');
-      res = await axiosInstance.post(URL_BUY, {
-        'validCode': pet.validCode,
-        'seed': captchaDate.seed,
-        'captcha': captcha,
-        'petId': pet.petId,
-        'requestId': Date.now(),
-        'amount': pet.amount,
-        'appId': 1,
-        'tpl': ''
-      });
-      console.log('购买结果', JSON.stringify(res.data));
+    } else {
+      // 人工识别
+      prompt.start();
+      const result = await getCaptcha();
+      captcha = result.captcha;
     }
-  } catch (err) {
-    console.warn(err.message);
+
+    // 买狗
+    console.log('开始买狗...');
+    res = await axiosInstance.post(URL_BUY, {
+      'validCode': pet.validCode,
+      'seed': captchaData.seed,
+      'captcha': captcha,
+      'petId': pet.petId,
+      'requestId': Date.now(),
+      'amount': pet.amount,
+      'appId': 1,
+      'tpl': ''
+    });
+    console.log('购买结果', JSON.stringify(res.data));
   }
 }
 
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
 let count = 0;
-setInterval(letUsGo, 2000); // 500 ms查询一次
+
+async function start() {
+  while (true) {
+    try {
+      await sleep(1500);
+      await letUsGo();
+    } catch (err) {
+      console.warn(err.message);
+    }
+  }
+}
+
+start();
